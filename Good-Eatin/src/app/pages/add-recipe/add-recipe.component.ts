5b7cb4d0aaa36ef709a7fef47059c38214ce5d0f
Dallin Wrathall <Dwrath299@gmail.com>
2019-01-31 14:47:37 -0700

    Recipe Form updates

--- Good-Eatin/src/app/pages/add-recipe/add-recipe.component.ts
+++ Good-Eatin/src/app/pages/add-recipe/add-recipe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../classes/recipe';
 
+
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
@@ -8,34 +9,52 @@ import { Recipe } from '../../classes/recipe';
 })
 export class AddRecipeComponent implements OnInit {
   recipe: Recipe;
-  constructor() { this.recipe = new Recipe(); }
+  ingredientCount: number;
+  instructionCount: number;
+
+  constructor() {
+      this.recipe = new Recipe();
+      this.ingredientCount = 2;
+      this.instructionCount = 2;
+    }
+
 
   ngOnInit() {
   }
 
   addIngredient() {
-      console.log('Im here');
-    const container = document.getElementById('ingredient-container');
-    const input1 = document.createElement('input');
-    input1.type = 'text';
-    input1.name = 'ingredient[]';
-    input1.addEventListener('change', this.addIngredient);
-    container.appendChild(input1);
-    const input2 = document.createElement('input');
-    input2.type = 'text';
-    input2.name = 'amount[]';
-    container.appendChild(input2);
-                // Append a line break
-    container.appendChild(document.createElement('br'));
+    if (this.ingredientCount < 20) {
+        const container = document.getElementById('ingredient-container');
+        const ingredientInput = document.createElement('input');
+        ingredientInput.type = 'text';
+        ingredientInput.name = 'ingredient[]';
+        ingredientInput.title = '1';
+
+        const amountInput = document.createElement('input');
+        amountInput.type = 'text';
+        amountInput.name = 'amount[]';
+        container.appendChild(ingredientInput);
+        container.appendChild(amountInput);
+                    // Append a line break
+        container.appendChild(document.createElement('br'));
+        this.ingredientCount++;
+    } else {
+        // Message
+    }
   }
 
   addInstruction() {
-    const container = document.getElementById('instruction-container');
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.name = 'instruction[]';
-    input.addEventListener('change', this.addInstruction);
-    container.appendChild(input);
+      if (this.instructionCount < 20) {
+        const container = document.getElementById('instruction-container');
+        const instructionInput = document.createElement('input');
+        instructionInput.type = 'text';
+        instructionInput.name = 'instruction[]';
+        container.appendChild(instructionInput);
+        container.appendChild(document.createElement('br'));
+        this.instructionCount++;
+      } else {
+          // Message
+      }
 
   }
 
---
+++ Good-Eatin/src/app/services/upload.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUpload {
+
+    key: string;
+    name: string;
+    url: string;
+    file: File;
+
+    constructor(file: File) {
+        this.file = file;
+    }
+}
+
+
+@Injectable()
+export class UploadService {
+
+    private basePath = '/uploads';
+
+    constructor(private db: AngularFirestore) { }
+
+    pushFileToStorage(fileUpload: FileUpload, progress: { percentage: number }, type: string, data: any) {
+        const storageRef = firebase.storage().ref();
+        const uploadTask = storageRef.child(`${this.basePath}/${fileUpload.file.name}`).put(fileUpload.file);
+
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {
+        // in progress
+        const snap = snapshot as firebase.storage.UploadTaskSnapshot;
+        progress.percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+      },
+      (error) => {
+        // fail
+        console.log(error);
+      },
+      () => {
+        // success
+        uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+          console.log('File available at', downloadURL);
+          fileUpload.url = downloadURL;
+          fileUpload.name = fileUpload.file.name;
+          this.saveFileData(fileUpload);
+        });
+      }
+    );
+  }
+
+  private saveFileData(fileUpload: FileUpload) {
+    this.db.list(`${this.basePath}/`).push(fileUpload);
+  }
+
+  getRecipe(id: string): {
+    
+  }
+
+  deleteFileUpload(fileUpload: FileUpload) {
+    this.deleteFileDatabase(fileUpload.key)
+      .then(() => {
+        this.deleteFileStorage(fileUpload.name);
+      })
+      .catch(error => console.log(error));
+  }
+
+  private deleteFileDatabase(key: string) {
+    return this.db.list(`${this.basePath}/`).remove(key);
+  }
+
+  private deleteFileStorage(name: string) {
+    const storageRef = firebase.storage().ref();
+    storageRef.child(`${this.basePath}/${name}`).delete();
+  }
+}
+}
+
+
+
+
+
+
+
