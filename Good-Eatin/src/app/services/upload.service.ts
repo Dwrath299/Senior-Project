5d269a788516ae8ea6e426d8c0a8d39d80e00dc5
Dallin Wrathall <Dwrath299@gmail.com>
2019-02-04 18:02:29 -0700

    Upload service should be good.

--- Good-Eatin/src/app/services/upload.service.ts
+++ Good-Eatin/src/app/services/upload.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+import { Recipe } from '../classes/recipe';
+import { User } from '../classes/user';
 
 @Injectable({
   providedIn: 'root'
@@ -23,8 +25,14 @@ export class FileUpload {
 export class UploadService {
 
     private basePath = '/uploads';
+    private userRef: AngularFirestoreCollection<User>;
 
-    constructor(private db: AngularFirestore) { }
+    private recipeRef: AngularFirestoreCollection<Recipe>;
+
+    constructor(private db: AngularFirestore) {
+        this.userRef = db.collection('users');
+        this.recipeRef = db.collection('recipes');
+    }
 
     pushFileToStorage(fileUpload: FileUpload, progress: { percentage: number }, type: string, data: any) {
         const storageRef = firebase.storage().ref();
@@ -46,37 +54,56 @@ export class UploadService {
           console.log('File available at', downloadURL);
           fileUpload.url = downloadURL;
           fileUpload.name = fileUpload.file.name;
-          this.saveFileData(fileUpload);
+          data.picture = fileUpload.url;
+          if (type = 'user') {
+            this.savePicToUser(data);
+          } else if (type = 'recipe') {
+            this.saveRecipe(data);
+          }
         });
       }
     );
   }
 
-  private saveFileData(fileUpload: FileUpload) {
-    this.db.list(`${this.basePath}/`).push(fileUpload);
+  private saveRecipe(data: Recipe) {
+      const id = data.id;
+      this.recipeRef.doc(id.toString()).ref.get()
+                        .then(doc => {
+                                // This recipe doesn't exist!
+                                if (!doc.exists) {
+                                    // Need to store the recipe
+                                    // Store it in cloud firestore
+                                    this.recipeRef.doc(id.toString()).set(Object.assign({}, data));
+                                    return true;
+                                } else {
+                                    // This deletes the old recipe and adds it again.
+                                    this.recipeRef.doc(id.toString()).delete().then(function() {
+                                         this.recipeRef.doc(id.toString()).set(Object.assign({}, data));
+                                    }).catch(function(error) {
+                                        console.error('Error removing document: ', error);
+                                    });
+                                    return false;
+                                }
+
+                        });
   }
 
-  getRecipe(id: string): {
-    
+  private savePicToUser(data: User) {
+      const id = data.id;
+      this.userRef.doc(id.toString()).ref.get()
+                      .then(doc => {
+                          if (doc.exists) {
+                              this.userRef.doc(id.toString()).update({
+                                  picture: data.picture
+                              });
+                          } else {
+                              console.error('Error updating user picture');
+                          }
+                      });
+
   }
 
-  deleteFileUpload(fileUpload: FileUpload) {
-    this.deleteFileDatabase(fileUpload.key)
-      .then(() => {
-        this.deleteFileStorage(fileUpload.name);
-      })
-      .catch(error => console.log(error));
-  }
 
-  private deleteFileDatabase(key: string) {
-    return this.db.list(`${this.basePath}/`).remove(key);
-  }
-
-  private deleteFileStorage(name: string) {
-    const storageRef = firebase.storage().ref();
-    storageRef.child(`${this.basePath}/${name}`).delete();
-  }
-}
 }
 
 
