8ddb3211b0a6c963f9b9210bab0073f0b48a17a1
Dallin Wrathall <Dwrath299@gmail.com>
2019-02-13 09:29:20 -0700

    Change to the week class

--- Good-Eatin/src/app/classes/week.ts
+++ Good-Eatin/src/app/classes/week.ts
@@ -1,9 +1,17 @@
-import { Meal } from './meal';
+
 export class Week {
     startDate: string;
-    meals: Array<Meal>;
-    constructor(startDate: string, meals: Array<Meal>) {
-        this.startDate = startDate;
-        this.meals = meals;
+    meals: [
+        {'Sunday', ['']},
+        {'Monday', ['']},
+        {'Tuesday', ['']},
+        {'Wednesday', ['']},
+        {'Thursday', ['']},
+        {'Friday', ['']},
+        {'Saturday', ['']}
+
+    ];
+    constructor() {
+        this.startDate = '';
     }
 }
