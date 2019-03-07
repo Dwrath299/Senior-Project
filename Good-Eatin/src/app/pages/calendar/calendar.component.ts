48c92800ee5af3dcbc20fb8147013c0d37ca6f5c
Dallin Wrathall <Dwrath299@gmail.com>
2019-03-07 07:37:13 -0700

    Calendar page doesn't need to refresh now.

--- Good-Eatin/src/app/pages/calendar/calendar.component.ts
+++ Good-Eatin/src/app/pages/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Recipe } from '../../classes/recipe';
 import { User } from '../../classes/user';
@@ -7,8 +7,6 @@ import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { async } from '@angular/core/testing';
 import { resolve } from 'url';
-
-
 
 @Component({
   selector: 'app-calendar',
@@ -27,7 +25,7 @@ export class CalendarComponent implements OnInit {
       this.weeks = [new Week()];
       // Next Week
       this.weeks.push(new Week());
-      let dayHolder;
+      
 
 
       this.userRef = db.collection('users');
@@ -36,64 +34,71 @@ export class CalendarComponent implements OnInit {
       localStorage.removeItem('recipeId');
       this.user = new User();
       this.user.id = localStorage.getItem('userId');
-      this.userRef.doc(this.user.id).ref.get().then( doc => {
-        this.user.friends = doc.data().friends;
-        this.user.picture = doc.data().picture;
-        this.user.recipes = doc.data().recipes;
-        this.user.weeks = doc.data().weeks;
-        let i = 0;
-        console.log(this.user.weeks);
-        for (let i = 0; i < this.user.weeks.length; i++) {
-          let weekID = this.user.weeks[i];
-          this.weekRef.doc(weekID).ref.get().then( weekDoc => {
-            console.log(weekID);
-            this.weeks[i].startDate = weekDoc.data().startDate;
-            // Get Sunday
-            dayHolder = weekDoc.data().sunday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].sunday = value;
-            });
-            // Get Monday
-            dayHolder = weekDoc.data().monday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].monday = value;
-            });
-            // Get Tuesday
-            dayHolder = weekDoc.data().tuesday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              console.log(i);
-              console.log(this.weeks[i]);
-              this.weeks[i].tuesday = value;
-            });
-            // Get Wednesday
-            dayHolder = weekDoc.data().wednesday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].wednesday = value;
-            });
-            // Get Thursday
-            dayHolder = weekDoc.data().thursday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].thursday = value;
-            });
-            // Get Friday
-            dayHolder = weekDoc.data().friday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].friday = value;
-            });
-            // Get Saturday
-            dayHolder = weekDoc.data().saturday;
-            this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-              this.weeks[i].saturday = value;
-            });
-            console.log(this.weeks[i]);
-          });
-        };
-      });
+      this.getCalendar();
+      
       
    }
 
   ngOnInit() {
   }
+
+getCalendar() {
+  let dayHolder;
+  this.userRef.doc(this.user.id).ref.get().then( doc => {
+    this.user.friends = doc.data().friends;
+    this.user.picture = doc.data().picture;
+    this.user.recipes = doc.data().recipes;
+    this.user.weeks = doc.data().weeks;
+    let i = 0;
+    console.log(this.user.weeks);
+    for (let i = 0; i < this.user.weeks.length; i++) {
+      let weekID = this.user.weeks[i];
+      this.weekRef.doc(weekID).ref.get().then( weekDoc => {
+        console.log(weekID);
+        this.weeks[i].startDate = weekDoc.data().startDate;
+        // Get Sunday
+        dayHolder = weekDoc.data().sunday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].sunday = value;
+        });
+        // Get Monday
+        dayHolder = weekDoc.data().monday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].monday = value;
+        });
+        // Get Tuesday
+        dayHolder = weekDoc.data().tuesday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          console.log(i);
+          console.log(this.weeks[i]);
+          this.weeks[i].tuesday = value;
+        });
+        // Get Wednesday
+        dayHolder = weekDoc.data().wednesday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].wednesday = value;
+        });
+        // Get Thursday
+        dayHolder = weekDoc.data().thursday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].thursday = value;
+        });
+        // Get Friday
+        dayHolder = weekDoc.data().friday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].friday = value;
+        });
+        // Get Saturday
+        dayHolder = weekDoc.data().saturday;
+        this.dayRecipes(dayHolder, this.recipeRef).then(value => {
+          this.weeks[i].saturday = value;
+        });
+        console.log(this.weeks[i]);
+      });
+    };
+  });
+
+}
 
 recipeClick(id: string) {
     localStorage.setItem('recipeId', id);
@@ -155,60 +160,8 @@ dayRecipes = function(day, recipeRef) {
   runGenerator() {
     const generateMeals = firebase.functions().httpsCallable('generateMeals');
     console.log(generateMeals({userId: this.user.id}));
-    let dayHolder;
-    this.userRef.doc(this.user.id).ref.get().then( doc => {
-      this.user.friends = doc.data().friends;
-      this.user.picture = doc.data().picture;
-      this.user.recipes = doc.data().recipes;
-      this.user.weeks = doc.data().weeks;
-      let i = 0;
-      console.log(this.user.weeks);
-      for (let i = 0; i < this.user.weeks.length; i++) {
-        let weekID = this.user.weeks[i];
-        this.weekRef.doc(weekID).ref.get().then( weekDoc => {
-          console.log(weekID);
-          this.weeks[i].startDate = weekDoc.data().startDate;
-          // Get Sunday
-          dayHolder = weekDoc.data().sunday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].sunday = value;
-          });
-          // Get Monday
-          dayHolder = weekDoc.data().monday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].monday = value;
-          });
-          // Get Tuesday
-          dayHolder = weekDoc.data().tuesday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            console.log(i);
-            console.log(this.weeks[i]);
-            this.weeks[i].tuesday = value;
-          });
-          // Get Wednesday
-          dayHolder = weekDoc.data().wednesday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].wednesday = value;
-          });
-          // Get Thursday
-          dayHolder = weekDoc.data().thursday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].thursday = value;
-          });
-          // Get Friday
-          dayHolder = weekDoc.data().friday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].friday = value;
-          });
-          // Get Saturday
-          dayHolder = weekDoc.data().saturday;
-          this.dayRecipes(dayHolder, this.recipeRef).then(value => {
-            this.weeks[i].saturday = value;
-          });
-          console.log(this.weeks[i]);
-        });
-      };
-    });
+    //this.snackBar.open("Currently generating your meals!");
+    this.getCalendar();
   }
 
 }
