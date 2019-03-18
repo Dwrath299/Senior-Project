9dd015c00911a706734380bd7667d37b94cb20e5
Dallin Wrathall <Dwrath299@gmail.com>
2019-03-18 09:59:28 -0600

    Started the shopping list page

--- Good-Eatin/src/app/pages/shopping-list/shopping-list.component.ts
+++ Good-Eatin/src/app/pages/shopping-list/shopping-list.component.ts
@@ -12,7 +12,8 @@ export class ShoppingListComponent implements OnInit {
   user: User;
   userRef: AngularFirestoreCollection<User>;
   shoppingListRef: AngularFirestoreCollection<any>;
-  ingredientList: any;
+  dateSelect : any;
+  ingredientList: [{startDate: "", ingredients: []}, {startDate: "", ingredients: []}];
 
   constructor(private db: AngularFirestore, private router: Router) { 
     this.user = new User();
@@ -21,15 +22,27 @@ export class ShoppingListComponent implements OnInit {
     this.shoppingListRef = db.collection('shoppingLists');
     this.userRef.doc(this.user.id).ref.get().then( doc => {
       let shoppingListIDs = doc.data().shoppingLists;
+      // Get each of the weeks for the shopping
       for (let i = 0; i < shoppingListIDs.length; i++) {
         let listID = shoppingListIDs[i];
         this.shoppingListRef.doc(listID).ref.get().then( listDoc => {
-          this.ingredientList = listDoc.data();
-
+          this.ingredientList[i].startDate = listDoc.data().startDate;
+          this.ingredientList[i].ingredients = listDoc.data().ingredients;
+          console.log(this.ingredientList);
         });
       }
+      this.dateSelect = this.ingredientList[0].startDate;
 
     });
+  }
+
+  format(date): String {
+    const tempDate = new Date(date);
+    return tempDate.toLocaleDateString();
+  }
+
+  checkBox(weekIndex, ingredientIndex) {
+
   }
 
   ngOnInit() {
